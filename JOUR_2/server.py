5faeadaf0605766291a38856897048cc2d968df3
hnorhad <hagennorhadian@gmail.com>
2024-03-12 07:45:21 -0400

    jour2

--- JOUR_2/server.py
+++ JOUR_2/server.py
@@ -15,7 +15,6 @@ def addition():
 	# nb2=int(nb2)
 	# res=nb1+nb2
 	# res=str(res)
-	# res = str(int(nb1) + int(nb1))
 
 	# return res
 
@@ -54,6 +53,59 @@ def division():
 	nb2=int(nb2)
 	res=nb1/nb2
 	res=str(int(res))
+
+	return res
+
+@app.route("/api/operation", methods=["POST"])
+def operation():
+
+	operation = request.form.get("operation")
+	nb1 = request.form.get("nombre1")
+	nb2 = request.form.get("nombre2")
+	nb1=int(nb1)
+	nb2=int(nb2)
+	# addition="+"
+	# soustraction="-"
+	# multiplication="*"
+	# division="/"
+
+
+
+	if(operation=="addition"):
+		# signe=addition
+		signe="+"
+		res=nb1+nb2
+	elif(operation=="soustraction"):
+		# signe=soustraction
+		signe="-"
+		res=nb1-nb2
+	elif(operation=="multiplication"):
+		# signe=multiplication
+		signe="*"
+		res=nb1*nb2
+	elif(operation=="division"):
+		# signe=division
+		signe="/"
+		res=nb1/nb2
+	else:
+		raise Exception("Please choose an operation")
+
+	# match operation:
+	# 	case "addition":
+	# 		res=nb1+nb2
+	# 	case "soustraction":
+	# 		res=nb1-nb2
+	# 	case "multiplication":
+	# 		res=nb1*nb2
+	# 	case "division":
+	# 		res=nb1/nb2
+	# 	case _:
+	# 		raise Exception("Please choose an operation")
+
+
+
+	res=f"{nb1} {signe} {nb2}={res}"
+	# res=str(nb1) + " " + operation + " " + str(nb2) + " = " + str(res)
 
 	return res
 
